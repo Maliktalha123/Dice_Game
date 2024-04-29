c0e65a6e8452f7cfa16bb38a9909354a5046ee0b
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-04-29 22:09:30 +0500

    Dice Game

---
+++ app.js
@@ -0,0 +1,19 @@
+let userNumber = document.getElementById("userNumber");
+let resultNumber = document.getElementById("resultNumber");
+let message = document.getElementById("message");
+
+function diceGame(number) {
+  userNumber.innerText = number;
+  let randomNumber = Math.ceil(Math.random() * 6);
+resultNumber.innerText= number
+if(number == randomNumber){
+    message.innerText= "Congrats ! You Win"
+    message.style.color = "green"
+
+}
+else{
+    message.innerText = "Sorry, You Loose "
+    message.style.color = "red"
+}
+
+}
